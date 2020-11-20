490d5b99583ccae5d67bee6a7ac4345f1b3c3662
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-11-20 11:11:17 -0500

    added foreach over array of people and a button in that

--- watchOsDynamicLists WatchKit Extension/ContentView.swift
+++ watchOsDynamicLists WatchKit Extension/ContentView.swift
@@ -6,17 +6,33 @@
 //
 
 import SwiftUI
-struct person {
-    <#fields#>
+struct Person: Identifiable {
+    var id = UUID()
+    var name: String
+    var age: Int
 }
 struct ContentView: View {
-    //variable that is selected person
-   // @State var selectedPerson
+    
+    //people array of Person structs
+    var people: [Person] = [
+        Person(name: "Chris", age: 5),
+        Person(name: "Alfred", age: 7),
+        Person(name: "Bruce", age: 9),
+        Person(name: "Wayne", age: 36)
+    ]
+    
+    //variable that is selected person optional of struct
+    @State var selectedPerson: Person?
+    
     var body: some View {
         //actual list
         List {
-            ForEach(0..<5) { _ in
-                Text("aaa")
+            ForEach(people, id: \.id) { thisPerson in
+                VStack {
+                    Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Button")/*@END_MENU_TOKEN@*/
+                    }
+                }
             }
         }
     }
